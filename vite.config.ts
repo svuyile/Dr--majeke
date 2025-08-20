a80d5660135277251c05cad72ed45a3dd50dee26
Sivuyile Manqumakazi <manqumakazimavhara@gmail.com>
2025-08-20 22:21:13 +0200

    Update asset path and Vite base config for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Dr--majeke/', // Use your repo name here
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
